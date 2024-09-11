adf65ffcc3b346660eba9f63e20a7802f745666b
Imran Asad <imranasad5252@gmail.com>
2024-09-11 22:24:05 +0500

    printing of alphabets

---
+++ alphabetsPattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    char ch;
+    for (int i = 0; i < n; i++)
+    {
+        ch='A';
+        for(int j=0;j<=n;j++)
+        {
+            cout<<ch;
+            ch=ch+1;
+        }
+        cout<<endl;
+    }
+    
+
+}
