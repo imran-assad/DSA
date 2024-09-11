b2ad2d81741000a9bcdf4bf6bcf6fedc3f534009
Imran Asad <imranasad5252@gmail.com>
2024-09-11 20:08:42 +0500

    working on conditions

--- test.cpp
+++ test.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello this is me imran";
+    char ch;
+    cout<<"Enter any character";
+    cin>>ch;
+    if(ch>=65 && ch<=90)
+    {
+        cout<<"Upper Case";
+    }
+    else{
+        cout<<"Lower case";
+
+    }
+
+    
+    
 }
