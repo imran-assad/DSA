dd52eba6c8030f70401f87db0faa6c6a0a0fa18a
Imran Asad <imranasad5252@gmail.com>
2024-09-12 01:26:41 +0500

    printing of continuous alphabets

--- continousNoPrint.cpp
+++ continousNoPrint.cpp
@@ -2,14 +2,24 @@
 using namespace std;
 int main()
 {
-    int num=1,n=5;
+    // int num=1,n=5;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout<<num<<" ";
+    //         num++;     
+    //     }
+    //     cout<<endl;     
+    // }    
+    char ch='A',n=5;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout<<num<<" ";
-            num++;     
+            cout<<ch<<" ";
+            ch=ch+1;    
         }
         cout<<endl;     
-    }    
+    }  
 }
