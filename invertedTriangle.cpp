f16f42361c5b8b646fa5cc06fcc490dff7eed4e6
Imran Asad <imranasad5252@gmail.com>
2024-09-13 01:04:14 +0500

    pattern printing for logic building

---
+++ invertedTriangle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num=1, n=5;
+    // for(int i=0; i<n;i++)
+    // {
+    //     for(int j=4;j>i;j--)
+    //     {
+    //         cout<<num<<" ";
+    //      }
+    //      num++;
+    //      cout<<endl;
+    //  }
+     for(int i=0; i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<i+1;
+         }
+         cout<<endl;
+     }
+}
+    // char ch='A';
+    // int n=5;
+    // for(int i=0; i<n;i++)
+    // {
+    //     for(int j=i+1;j>0;j--)
+    //     {
+    //         cout<<ch<<" ";
+    //         ch++;
+    //     }
+    //     cout<<endl;
+    // }
+
