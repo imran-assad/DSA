4df92103ecce033fa36ff08de7179ed9e2e49765
Imran Asad <imranasad5252@gmail.com>
2024-09-13 01:22:32 +0500

    inverted + pyramid pattern for alphabets

--- invertedTriangle.cpp
+++ invertedTriangle.cpp
@@ -24,6 +24,37 @@ int main()
          }
          cout<<endl;
      }
+   char  ch='A';
+      for(int i=0; i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<ch;
+         }
+         ch++;
+         cout<<endl;
+
+     }
+
+      for(int i=0; i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<ch<<" ";
+         }
+         ch++;
+         cout<<endl;
+
+     }
+     
 }
     // char ch='A';
     // int n=5;
