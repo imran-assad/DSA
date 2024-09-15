924132a0c6488727428e765bde557577d373e4e9
Imran Asad <imranasad5252@gmail.com>
2024-09-15 19:12:25 +0500

    data modifiers

---
+++ dataTypeModifiers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(signed)<<endl;
+    cout<<sizeof(unsigned)<<endl;
+    cout<<sizeof(short)<<endl;
+    cout<<sizeof(long long)<<endl;
+    cout<<sizeof(double);
+}
